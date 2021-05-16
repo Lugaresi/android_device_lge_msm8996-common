798beb9c5acd2fba74d0dfd9fd864661e58f7ee6
Lugaresi <root@stukamanhpt4.com>
2021-05-16 22:13:02 +0200

    Redo: kang doze from op3

--- doze/src/org/lineageos/settings/doze/DozeService.java
+++ doze/src/org/lineageos/settings/doze/DozeService.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,14 +29,14 @@ public class DozeService extends Service {
     private static final String TAG = "DozeService";
     private static final boolean DEBUG = false;
 
-    private ProximitySensor mProximitySensor;
-    private TiltSensor mTiltSensor;
+    private PickupSensor mPickupSensor;
+    private PocketSensor mPocketSensor;
 
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
-        mProximitySensor = new ProximitySensor(this);
-        mTiltSensor = new TiltSensor(this);
+        mPickupSensor = new PickupSensor(this);
+        mPocketSensor = new PocketSensor(this);
 
         IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
@@ -53,8 +54,8 @@ public class DozeService extends Service {
         if (DEBUG) Log.d(TAG, "Destroying service");
         super.onDestroy();
         this.unregisterReceiver(mScreenStateReceiver);
-        mProximitySensor.disable();
-        mTiltSensor.disable();
+        mPickupSensor.disable();
+        mPocketSensor.disable();
     }
 
     @Override
@@ -64,23 +65,23 @@ public class DozeService extends Service {
 
     private void onDisplayOn() {
         if (DEBUG) Log.d(TAG, "Display on");
-        if (Utils.pickUpEnabled(this)) {
-            mTiltSensor.disable();
+        if (Utils.isPickUpEnabled(this)) {
+            mPickupSensor.disable();
         }
-        if (Utils.handwaveGestureEnabled(this) ||
-                Utils.pocketGestureEnabled(this)) {
-            mProximitySensor.disable();
+        if (Utils.isHandwaveGestureEnabled(this) ||
+                Utils.isPocketGestureEnabled(this)) {
+            mPocketSensor.disable();
         }
     }
 
     private void onDisplayOff() {
         if (DEBUG) Log.d(TAG, "Display off");
-        if (Utils.pickUpEnabled(this) && Utils.tiltAlwaysEnabled(this)) {
-            mTiltSensor.enable();
+        if (Utils.isPickUpEnabled(this)) {
+            mPickupSensor.enable();
         }
-        if (Utils.proximityAlwaysEnabled(this) && (Utils.handwaveGestureEnabled(this) ||
-                Utils.pocketGestureEnabled(this))) {
-            mProximitySensor.enable();
+        if (Utils.isHandwaveGestureEnabled(this) ||
+                Utils.isPocketGestureEnabled(this)) {
+            mPocketSensor.enable();
         }
     }
 
